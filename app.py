3313d881eb80d4070a2fa8d64abc8e94904fa36c
Edmilson-Filimone <75740869+Edmilson-Filimone@users.noreply.github.com>
2021-06-24 13:51:36 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def inserir(nome, titulo, data, ficheiro, imagem_1, imagem_2):
     usuario atraves dos seus parametros"""
 
     try:
-        conexao = sqlite3.connect('BASE.db')
+        conexao = sqlite3.connect('Documents\BASE.db')
         cursor = conexao.cursor()
         create = "CREATE TABLE IF NOT EXISTS One " \
                  "(nome text,titulo text," \
@@ -33,6 +33,7 @@ def inserir(nome, titulo, data, ficheiro, imagem_1, imagem_2):
         conexao.commit()
     except Exception as erro:
         print(erro)
+        print('o bife esta aqui')
     finally:
         conexao.close()
 
@@ -43,11 +44,12 @@ def selet_box():
      uma lista de listas [[],[],...] composta por elementos de cada coluna"""
 
     try:
-        conexao = sqlite3.connect('BASE.db')
+        conexao = sqlite3.connect('Documents\BASE.db')
         cursor = conexao.cursor()
         select = "SELECT * FROM One"
         cursor.execute(select)
         retorno = cursor.fetchall()
+        print(retorno)
         nome_lista = []
         titulo_lista = []
         data_lista = []
@@ -56,6 +58,7 @@ def selet_box():
             nome_lista.append(tupla[0])  # guardando os dados da coluna nome na lista
             titulo_lista.append(tupla[1])  # guardando os dados da coluna titulo na lista
             data_lista.append(tupla[2])  # guardando os dados da coluna data na lista
+            print(nome_lista, '\n', titulo_lista, '\n', data_lista)
         select_box_lista = [nome_lista, titulo_lista, data_lista]
         return select_box_lista
     except:
@@ -71,11 +74,12 @@ documento = ''
 def query(nome, titulo, data):
     try:
         global documento
-        conexao = sqlite3.connect('BASE.db')
+        conexao = sqlite3.connect('Documents\BASE.db')
         cursor = conexao.cursor()
         select = "SELECT ficheiro FROM One WHERE nome == (?) AND titulo == (?) AND data = (?)"
         cursor.execute(select, (nome,titulo, data))
         retorno = cursor.fetchall()
+        print(retorno)
 
         # Para o documento
         for tupla in retorno:
@@ -83,6 +87,7 @@ def query(nome, titulo, data):
 
         with open('page_1.csv', 'wb') as file_1:
             done = file_1.write(documento)
+            print(f'Documento decode_output: {done}')
             file_1.close()
 
         df = pd.read_csv('page_1.csv')
@@ -100,11 +105,12 @@ img_1 = ''
 def query_2(nome, titulo, data):
     global img_1
     try:
-        conexao = sqlite3.connect('BASE.db')
+        conexao = sqlite3.connect('Documents\BASE.db')
         cursor = conexao.cursor()
         select = "SELECT imagem_1 FROM One WHERE nome == (?) AND titulo == (?) AND data = (?)"
         cursor.execute(select, (nome, titulo, data))
         retorno = cursor.fetchall()
+        #print(retorno)
 
         # Para a imagem 1
         for tupla in retorno:
@@ -112,6 +118,7 @@ def query_2(nome, titulo, data):
 
         with open('i1.jpg', 'wb') as file_2:
             image_1 = file_2.write(img_1)
+            print(f'Imagem_1 decode_output: {image_1}')
             file_2.close()
         pil = Image.open('i1.jpg')
         st.header('Vizualizacao do Gel')
@@ -130,11 +137,12 @@ img_2 = ''
 def query_3(nome, titulo, data):
     global img_2
     try:
-        conexao = sqlite3.connect('BASE.db')
+        conexao = sqlite3.connect('Documents\BASE.db')
         cursor = conexao.cursor()
         select = "SELECT imagem_2 FROM One WHERE nome == (?) AND titulo == (?) AND data = (?)"
         cursor.execute(select, (nome, titulo, data))
         retorno = cursor.fetchall()
+        #print(retorno)
 
         # Para a imagem 2
         for tupla in retorno:
@@ -142,6 +150,7 @@ def query_3(nome, titulo, data):
 
         with open('image_2.jpg', 'wb') as file_3:
             image_2 = file_3.write(img_2)
+            print(f'Imagem_2 decode_output: {image_2}')
             file_3.close()
         pil = Image.open('image_2.jpg')
         st.write('')
@@ -164,7 +173,6 @@ def jpg():
 
 # Main Window
 st.title("RESULTADOS DA PCR")
-st.write(" ")
 #st.image(image=jpg())
 txt = st.subheader('---------------[ Selecione uma das opcoes no Menu ]------------------')
 
@@ -208,4 +216,5 @@ if pesquisa:
         query_2(nome=str(sb_nome), titulo=str(sb_titulo), data=str(sb_data))
         query_3(nome=str(sb_nome), titulo=str(sb_titulo), data=str(sb_data))
         txt.text('-------- Veja os resultados abaixo, caso nada apareca, entao nao ha nenhuma informacao disponivel --------')
-
+#conda activate I:\Biotech-School\Web_Dev\web\envs\one
+#streamlit run I:\Biotech-School\Web_Dev\Gestor.py
